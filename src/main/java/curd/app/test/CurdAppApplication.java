f7c856ca1c9ea78ca2873fdb2afb812e0ea2e134
kuldeep kumar <kuldeep.kumar@oodlestechnologies.com>
2018-04-27 13:16:11 +0530

    first commit

---
+++ src/main/java/curd/app/test/CurdAppApplication.java
@@ -0,0 +1,12 @@
+package curd.app.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdAppApplication.class, args);
+	}
+}
